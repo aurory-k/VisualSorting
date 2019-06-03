cbfecdec9e6133ec5e0187db15154a50c92a3780
Travis Kovacic <tkovaci1@ford.com>
2019-06-03 09:24:14 -0400

    Added multiple drawing for comparing sorts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,9 @@ import sorter.bubbleSort
 import sorter.insertionSort
 import sorter.startMergeSort
 import sorter.startQuickSort
+import java.awt.Color
 import java.awt.GraphicsEnvironment
+import java.awt.GridLayout
 import javax.swing.JFrame
 
 var gd = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice!!
@@ -15,10 +17,23 @@ fun main() {
     frame.isVisible = true
     frame.setSize(screenWidth, screenHeight)
 
-    val canvas = SortingCanvas(1000)
-    canvas.setSize(screenWidth, screenHeight)
+    val canvas = SortingCanvas(4, 100, Color.BLUE)
+    canvas.setSize(screenWidth / 2, screenHeight / 2)
+
+    val canvas2 = SortingCanvas(4, 100, Color.GREEN)
+    canvas2.setSize(screenWidth / 2, screenHeight / 2)
+
+    val canvas3 = SortingCanvas(4, 100, Color.RED)
+    canvas3.setSize(screenWidth / 2, screenHeight / 2)
+
+    val canvas4 = SortingCanvas(4, 100, Color.MAGENTA)
+    canvas4.setSize(screenWidth / 2, screenHeight / 2)
 
     frame.contentPane.add(canvas)
+    frame.contentPane.add(canvas2)
+    frame.contentPane.add(canvas3)
+    frame.contentPane.add(canvas4)
+    frame.layout = GridLayout(2, 2)
 
 //    canvas.collectionOfBars.bubbleSort { bars ->
 //        Thread.sleep(10)
@@ -38,11 +53,36 @@ fun main() {
 //        canvas.repaint()
 //    }
 
+    Thread {
         canvas.collectionOfBars.startQuickSort { bars ->
-        Thread.sleep(10)
-        canvas.updateBarList(bars)
-        canvas.repaint()
-    }
+            Thread.sleep(10)
+            canvas.updateBarList(bars)
+            canvas.repaint()
+        }
+    }.start()
+    Thread {
+        canvas2.collectionOfBars.insertionSort { bars ->
+            Thread.sleep(10)
+            canvas2.updateBarList(bars)
+            canvas2.repaint()
+        }
+    }.start()
+
+    Thread {
+        canvas3.collectionOfBars.startMergeSort { bars ->
+            Thread.sleep(10)
+            canvas3.updateBarList(bars)
+            canvas3.repaint()
+        }
+    }.start()
+
+    Thread {
+        canvas4.collectionOfBars.bubbleSort { bars ->
+            Thread.sleep(10)
+            canvas4.updateBarList(bars)
+            canvas4.repaint()
+        }
+    }.start()
 
 
 }
--- src/main/kotlin/SortingCanvas.kt
+++ src/main/kotlin/SortingCanvas.kt
@@ -5,14 +5,17 @@ import java.awt.geom.Rectangle2D
 import javax.swing.JPanel
 import kotlin.random.Random.Default.nextDouble
 
-class SortingCanvas(private val numberOfBars: Int) : JPanel() {
+class SortingCanvas(private val numberOfSorts: Int = 1, private val numberOfBars: Int = 500, private val color: Color = Color.BLUE) : JPanel() {
 
     var collectionOfBars = listOf<Bar>()
 
+    private val canvasWidth = screenWidth.toDouble() / 2
+    private val canvasHeight = screenHeight.toDouble() / 2
+
     init {
-        val barWidth = screenWidth.toDouble() / numberOfBars.toDouble()
+        val barWidth = canvasWidth / numberOfBars.toDouble()
         (0..numberOfBars).forEach { _ ->
-            val bar = Bar(barWidth, nextDouble(100.0, screenHeight - 50.0), Color.BLUE)
+            val bar = Bar(barWidth, nextDouble(100.0, (canvasHeight - 50.0)), color)
             collectionOfBars = collectionOfBars.plus(bar)
         }
     }
@@ -26,7 +29,7 @@ class SortingCanvas(private val numberOfBars: Int) : JPanel() {
         val graphics2d = g as Graphics2D
 
         collectionOfBars.forEachIndexed { index, bar ->
-            val rect = Rectangle2D.Double(index * bar.width, screenHeight - bar.height, bar.width, bar.height)
+            val rect = Rectangle2D.Double(index * bar.width, canvasHeight - bar.height, bar.width, bar.height)
             graphics2d.color = bar.color
             graphics2d.fill(rect)
             graphics2d.color = Color.BLACK
